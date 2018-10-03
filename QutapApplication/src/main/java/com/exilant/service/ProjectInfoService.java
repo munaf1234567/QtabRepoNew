01ca1ae2da0a6940f97e534e6476a7d2da6f2649
Chandni Verma <chandniverma0204@gmail.com>
2018-10-03 11:51:41 +0530

    chandni

---
+++ QutapApplication/src/main/java/com/exilant/service/ProjectInfoService.java
@@ -0,0 +1,20 @@
+package com.exilant.service;
+
+import java.util.List;
+
+import com.exilant.CommonUtils.Response;
+import com.exilant.model.ProjectInfoModel;
+
+public interface ProjectInfoService {
+
+	Response saveProjectInfo(ProjectInfoModel projectInfoModel) ;
+	
+	public ProjectInfoModel getProjectInfo(String projectId);
+
+	public List<ProjectInfoModel> getProjectListInfo();
+
+	Response updateProjectInfo(ProjectInfoModel projectInfoModel);
+
+	Response deleteProjectInfo(String projectId);
+
+}
